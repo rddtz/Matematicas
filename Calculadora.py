34dff288321007de385367cf9f1b7dbfe348a0a5
rddtz <rayandematos@gmail.com>
2023-03-03 07:45:49 -0300

    Tentei fazer o commit pelo PyCharm mas deu um bilhão de erro eba, vou continuar fazendo normal msm, só documentei e adicionei alguns comentarios pra faciliat o entendimento futuro

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,10 @@
 import sys
 
 def transformarEmNum(item):
+    '''Retorna os itens da lista convertidos para números caso for possível, se forem stringgs como + ou - ele
+    apenas retorna a string, também caso ele ache parenteses ele apagara ele pra depois os parenteses serem readicionados,
+    isso por que ao escrever (10 isso se torna uma string e é preciso separar o ( do dez, deletando o parenteses e rechamando a funçaõ'''
+
     try:
         item = eval(item)
         return item
@@ -10,7 +14,7 @@ def transformarEmNum(item):
             if listaAux[0] == '(':
                 del listaAux[0]
             listaAux = ''.join(listaAux)
-            item = transformarEmNum(listaAux)
+            item = transformarEmNum(listaAux) #Chama a funcção novamente depois de separar os parenteses
             return item
         elif ')' in item:
             listaAux = list(item)
@@ -18,12 +22,13 @@ def transformarEmNum(item):
             if listaAux[final] == ')':
                 del listaAux[final]
             listaAux = ''.join(listaAux)
-            item = transformarEmNum(listaAux)
+            item = transformarEmNum(listaAux) #Chama a função novamente depois de separar os parenteses
             return item
         else:
             return item
 
 def calcular(parametro, valorDef, n):
+    '''Calcula as contas normalmente baseado nas strings de sinalização (+, -, x ou /)'''
     lista = parametro
     if lista[n] == '-':
         valorDef -= lista[n + 1]
@@ -39,16 +44,20 @@ def calcular(parametro, valorDef, n):
         valorDef *= lista[n + 1]
     else:
         if valorDef == 0 and n == 0:
-            valorDef += lista[n]
+            valorDef += lista[n] #Caso seja o primeiro item e seja positivo
         else:
             pass
     return valorDef
 
 def prioridadeParenteses():
+    '''Chama as funções para adicionar novamente os parenteses, primerios os ( e depois os ), com base nos locais
+    que foram salvos pela função localdaPrioridadeParenteses'''
     adicionarParentesesInic()
     adicionarParentesesFim()
 
 def localdaPrioridadeParenteses():
+    '''Salva o local dos parenteses na lista, para depois que forem removidos pela função transformarEmNum() possam ser
+    adicionados novamente no lugar exato'''
     for n in range(len(lista)):
         try:
             if '(' in lista[n]:
@@ -84,25 +93,27 @@ def localDoFinalDoParenAtual():
             return n
 
 def calcularParenteses():
+    '''Isola e calcula os números que ficam entre os parenteses, depois apaga os parenteses e insere o resultado na lista no
+    lugar onde se encontrava a conta do parenteses'''
     contador = 0
     while True:
         if lista[contador] == '(':
             resultado = 0
-            localF = localDoFinalDoParenAtual()
+            localF = localDoFinalDoParenAtual() #Ve onde acaba o parenteses que esta sendo trabalhando pra pegar tudo dentro dele
             totalDeVezes = localF - contador
-            listaAux = []
+            listaAux = [] #lista pra isolar os números e calcula-los separados
             for loop in range(totalDeVezes):
-                del lista[contador]
+                del lista[contador] #apagar a conta dentro do () da lista para adicionar o resultado no lugar
                 listaAux.append(lista[contador])
             del listaAux[len(listaAux) - 1]
             del lista[contador]
-            listaAux = calcularPrioridade(listaAux)
+            listaAux = calcularPrioridade(listaAux) #Caso haja prioridade de X ou / dentro dos parenteses irá ser feito aqui
             for n in range(len(listaAux)):
-                resultado = calcular(listaAux, resultado, n)
-            lista.insert(contador, resultado)
-            contador = 0
+                resultado = calcular(listaAux, resultado, n) #Faz a conta normal com o que sobrou
+            lista.insert(contador, resultado) #Adiciona o resultado na lista
+            contador = 0 #Reseta o contador pra ele voltar do ínicio pois a lista mudou de tamanho
         contador += 1
-        if TrOuFalseParaParent(lista) == True:
+        if TrOuFalseParaParent(lista) == True: #Ve se ainad há parenteses na conta, se não tiver vai acabar o loop
             pass
         else:
             break
@@ -139,7 +150,7 @@ def prioridadeVezes(parametro):
     listaPriovezes = parametro
     local = localdaPrioridadeVez(listaPriovezes)
     resultado = listaPriovezes[local - 1]*listaPriovezes[local + 1]
-    deletarDaLista(listaPriovezes, local)
+    deletarDaLista(listaPriovezes, local) #Deleta os itens da lista para adicionar o resultado no local
     listaPriovezes.insert(local - 1, resultado)
     return listaPriovezes
 
@@ -151,11 +162,13 @@ def prioridadeDivisao(parametro):
     except:
         print('É impossivel dividir um número por 0')
         sys.exit()
-    deletarDaLista(listaPrioDiv, local)
+    deletarDaLista(listaPrioDiv, local) #Deleta os itens da lista para adicionar o resultado no local
     listaPrioDiv.insert(local - 1, resultado)
     return listaPrioDiv
 
 def calcularPrioridade(parametro):
+    '''Analisa a lista recebi pelo parametro, ve se há multiplicação ou divisão, caso tenha procura o local, faz o calculo dos
+    números, apaga os originais e adiciona o resultado, similar ao o que acontece com os parenteses'''
     listaDePrioridade = parametro
     contadorDeOperacoes = 0
     analiseSeJaFezTudo = list(filter(lambda item: item == 'x' or item == '/', listaDePrioridade))
@@ -166,19 +179,20 @@ def calcularPrioridade(parametro):
                 listaDePrioridade = prioridadeVezes(listaDePrioridade)
                 tamanhoLista = len(listaDePrioridade)
                 contadorDeOperacoes += 1
-                break
+                break #Esse break quebra o For, pra voltar a correr do início da lista sempre que uma operação for realizada
             if n == '/':
                 listaDePrioridade = prioridadeDivisao(listaDePrioridade)
                 tamanhoLista = len(listaDePrioridade)
                 contadorDeOperacoes += 1
                 break
 
-        if len(analiseSeJaFezTudo) == contadorDeOperacoes:
+        if len(analiseSeJaFezTudo) == contadorDeOperacoes: #ve se todas operações que recebem prioridade ja foram realizado
             return listaDePrioridade
         else:
             pass
 
 def arrumarZeroFlutuante(valor):
+    'Só pra transformar 5.0 em 5'
     valorInt = int(valor)
     vDez = valor * 10
     vIntDez = valorInt * 10
@@ -188,19 +202,23 @@ def arrumarZeroFlutuante(valor):
     else:
         return valor
 #---------------------------------
-localInicio = []
-localFim = []
-lista = input('Digite números: ')
+
+#Início do programa
+
+localInicio = [] #Variavel pra guardar o local dos parenteses '(', em geral tudo que tem inicio se refere a isso
+localFim = [] #Mesma coisa que o inicio, mas para ')'
+
+lista = input('Digite números: ') #Recebe a contaa
 lista = lista.split(' ')
-lista = list(filter(lambda item: item != '', lista))
+lista = list(filter(lambda item: item != '', lista)) #Remove os espaços
 localdaPrioridadeParenteses()
-lista = list(map(transformarEmNum, lista))
-lista = list(filter(lambda item: item != '', lista))
-soPraManter = lista
+lista = list(map(transformarEmNum, lista)) #Transforma os itens da lista em números inteiros/float se for possivel
+lista = list(filter(lambda item: item != '', lista)) #Remove os espaços novamente, pra caso haja espaço entre os parenteses não tenha erro
 
 total = 0
 tamanhoLista = len(lista)
-#ver se há parenteses
+
+#ver se há parenteses na conta e já faz os calcúlos necessários
 if len(localInicio) > 0 and len(localFim) > 0:
     prioridadeParenteses()
     calcularParenteses()
@@ -209,7 +227,8 @@ if len(localInicio) > 0 and len(localFim) > 0:
 lista = calcularPrioridade(lista)
 
 for n in range(tamanhoLista):
-    total = calcular(lista, total, n)
+    #calcula todos itens restantes da lista
+    total = calcular(lista, total, n) #Total envia a ele mesmo coo parametro, por isso não reseta a cada chamada
 
 total = arrumarZeroFlutuante(total)
 print(f'O resultado é {total}')
